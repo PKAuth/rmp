2f0fc862ee68af79db61bb98a976dd6e390b57cc
James Parker <dev@jamesparker.me>
2016-05-26 13:46:21 -0400

    add_positive_mut implemented

--- src/rmp/add.rs
+++ src/rmp/add.rs
@@ -15,7 +15,20 @@ impl Integer {
 
 	// Mutable borrowed addition.
 	pub fn add_mut( &mut self, rhs : &Integer) {
-		panic!("TODO")
+		match ( self.is_positive(), rhs.is_positive()) {
+			(true, true) => {
+				add_positives_mut( self, rhs);
+			},
+			(false, false) => {
+				add_positives_mut( self, rhs);
+			},
+			(true, false) => {
+				panic!("TODO")
+			},
+			(false, true) => {
+				panic!("TODO")
+			},
+		}
 	}
 }
 
@@ -23,66 +36,23 @@ impl Add for Integer {
 	type Output = Integer;
 
 	fn add( self, rhs : Integer) -> Integer {
-		fn add_positives( lhs : Integer, rhs : Integer) -> Vec<Block> {
-			let mut cs = Vec::with_capacity( max( lhs.capacity(), rhs.capacity())); // TODO: Improve this capacity?? Do we need to zero out memory? XXX
-			let mut i = 0;
-			let mut c = false;
-			let self_s = lhs.size();
-			let rhs_s = rhs.size();
-			let s = min( self_s, rhs_s);
-			while i < s {
-				let (mut x, d) = lhs.content[i].overflowing_add( rhs.content[i]);
-
-				if c {
-					let (y, e) = x.overflowing_add( 1);
-					c = d || e;
-					x = y
-				}
-				else {
-					c = d
-				}
-
-				cs.push( x);
-				i += 1;
-			}
-
-			let (g,s) = if self_s > rhs_s { (lhs, self_s)} else { (rhs, rhs_s)};
-			while i < s {
-				if c {
-					let (y, e) = g.content[i].overflowing_add( 1);
-					cs.push(y);
-					c = e;
-				}
-				else {
-					// Optimisation: we can break here and copy the rest of the bits? XXX
-					cs.push( g.content[i]);
-					c = false;
-				}
-
-				i += 1;
-			}
-
-			if c {
-				cs.push( 1);
-			}
-
-			cs
-		}
-
-		match ( self.is_positive(), rhs.is_positive()) {
-			(true, true) => {
-				let cs = add_positives( self, rhs);
-				pos_integer( cs)
-			},
-			(false, false) => {
-				let cs = add_positives( self, rhs);
-				neg_integer(cs)
-			},
-			(true, false) =>
-				sub_positives( &self, &rhs.neg()),
-			(false, true) =>
-				sub_positives( &rhs, &self.neg()),
-		}
+		let mut r = self;
+		r.add_mut( &rhs);
+		r
+		// match ( self.is_positive(), rhs.is_positive()) {
+		// 	(true, true) => {
+		// 		let cs = add_positives( self, rhs);
+		// 		pos_integer( cs)
+		// 	},
+		// 	(false, false) => {
+		// 		let cs = add_positives( self, rhs);
+		// 		neg_integer(cs)
+		// 	},
+		// 	(true, false) =>
+		// 		sub_positives( &self, &rhs.neg()),
+		// 	(false, true) =>
+		// 		sub_positives( &rhs, &self.neg()),
+		// }
 	}
 }
 
@@ -128,3 +98,62 @@ fn sub_positives( l : &Integer, r : &Integer) -> Integer {
 		neg_integer( cs)
 	}
 }
+
+fn add_positives_mut( lhs : &mut Integer, rhs : &Integer) {
+	let mut c = false;
+	let mut i = 0;
+	let self_s = lhs.size();
+	let rhs_s = rhs.size();
+	let s = min( self_s, rhs_s);
+	while i < s {
+		let (mut x, d) = lhs.content[i].overflowing_add( rhs.content[i]);
+	
+		if c {
+			let (y, e) = x.overflowing_add( 1);
+			c = d || e;
+			x = y;
+		}
+		else {
+			c = d;
+		}
+
+		if i < self_s {
+			lhs.content[i] = x;
+		}
+		else {
+			lhs.content.push( x);
+		}
+
+		i += 1;
+	}
+
+	// Check if lhs length is longer than rhs length.
+	if self_s >= rhs_s {
+		while i < self_s && c {
+			let (y, e) = lhs.content[i].overflowing_add( 1);
+			lhs.content[i] = y;
+			c = e;
+
+			i += 1;
+		}
+	}
+	else {
+		while i < rhs_s {
+			if c {
+				let (y, e) = rhs.content[i].overflowing_add( 1);
+				lhs.content.push( y);
+				c = e;
+			}
+			else {
+				lhs.content.push( rhs.content[i]);
+			}
+
+			i += 1;
+		}
+	}
+
+	if c {
+		lhs.content.push( 1);
+	}
+}
+
